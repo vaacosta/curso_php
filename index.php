b28c60bf08a970a2d8018a3c07b74cecc06f77e8
Victor A. Acosta <vaacosta@hotmail.com>
2015-07-28 19:39:57 -0500

    Configuras conexion a la BD
    Se ha configura la conexion a la Base de Datos

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-echo "hola Mundo";
+echo "CONEXION A LA BASE DE DATOS";
+$URL = "sql2.freemysqlhosting.net";
+$database = "sql285298";
+$usuario = "sql285298";
+$password = "bJ1*gX6%";
+$port = 3306;
+
+//CREAR CONECCION
+$conexion = mysqli_connect(
+	$URL,
+	$usuario,
+	$password,
+	$database,
+	$port);
+
+if(!$conexion) {
+	die(FINALIZO LA CONEXION);
+}
+echo "CONEXION EXITOSA";
 ?>
